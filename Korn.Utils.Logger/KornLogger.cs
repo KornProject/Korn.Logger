a8863828cb0f16650cf2ff3b583dccf38281fb57
Yotic <yoticcitoy@gmail.com>
2024-12-13 16:51:08 +0300

    Added exceptions for Logger

--- Korn.Utils.Logger/KornLogger.cs
+++ Korn.Utils.Logger/KornLogger.cs
@@ -1,6 +1,4 @@
-﻿using Korn.Utils.System;
-using System.Diagnostics;
-using System.Reflection;
+﻿using System.Reflection;
 
 namespace Korn.Utils.Logger;
 public static class KornLogger
@@ -50,15 +48,44 @@ public static class KornLogger
 
     static void Initialize()
     {
-        AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        AppDomain.CurrentDomain.UnhandledException += UnhandledException;
 
-        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        void UnhandledException(object sender, UnhandledExceptionEventArgs e)
         {
             var exception = e.ExceptionObject as Exception;
             if (exception is null)
                 return;
 
-            WriteUnhandledException(exception);
+            switch (exception)
+            {
+                case KornException kornException:
+                    if (kornException.ShowMessageBox)
+                        Exception(kornException);
+                    else WriteException(kornException);
+                    break;
+
+                case KornExpectedException kornExpectedException:
+                    if (kornExpectedException.ShowMessageBox)
+                        ExpectedException(kornExpectedException);
+                    else WriteExpectedException(kornExpectedException);
+                    break;
+
+                case KornUnexpectedError kornError:
+                    if (kornError.ShowMessageBox)
+                        UnxepectedError(kornError.Message);
+                    else WriteUnexpectedError(kornError.Message);
+                    break;
+
+                case KornError kornExpectedError:
+                    if (kornExpectedError.ShowMessageBox)
+                        Error(kornExpectedError.Message);
+                    else WriteError(kornExpectedError.Message);
+                    break;
+
+                default:
+                    WriteUnhandledException(exception);
+                    break;
+            }
         }
     }
 
@@ -89,45 +116,110 @@ public static class KornLogger
 
     public static void WriteExpectedException(Exception exception) => WriteLine("[Expected exception] " + exception.ToString());
 
+    public static void WriteUnexpectedError(string errorMessage, bool appendStacktrace = false)
+        => WriteLine("[Unexpected error] " + errorMessage + (appendStacktrace ? $"Stacktrace: \n{Environment.StackTrace}" : ""));
+
     public static void WriteError(string errorMessage, bool appendStacktrace = false)
-        => WriteLine("[Error] " + errorMessage + (appendStacktrace ? $"Stacktrace: \n{Environment.StackTrace}" : ""));
+    => WriteLine("[Error] " + errorMessage + (appendStacktrace ? $"Stacktrace: \n{Environment.StackTrace}" : ""));
 
-    public static void WriteExpectedError(string errorMessage, bool appendStacktrace = false)
-    => WriteLine("[Expected error] " + errorMessage + (appendStacktrace ? $"Stacktrace: \n{Environment.StackTrace}" : ""));
-
+    public static void Message(string[] messageLines) => Message(string.Join('\n', messageLines));
     public static void Message(string message)
     {
         WriteMessage(message);
-        Interop.MessageBox(message, "Message");
+        Interop.MessageBox(message, "Korn Message");
     }
 
+    public static void Error(string[] messageLines) => UnxepectedError(string.Join(' ', messageLines));
+    public static void UnxepectedError(string message)
+    {
+        WriteUnexpectedError(message);
+        Interop.MessageBox(message, "Korn unexpected error");
+    }
+
+    public static void ExpectedError(string[] messageLines) => Error(string.Join(' ', messageLines));
     public static void Error(string message)
     {
         WriteError(message);
-        Interop.MessageBox(message, "Error");
-    }
-
-    public static void ExpectedError(string message)
-    {
-        WriteExpectedError(message);
-        Interop.MessageBox(message, "Expected error");
+        Interop.MessageBox(message, "Korn error");
     }
 
     public static void Exception(Exception exception)
     {
         WriteException(exception);
-        Interop.MessageBox(exception.ToString(), "Exception");
+        Interop.MessageBox(exception.ToString(), "Korn exception");
     }
 
     public static void ExpectedException(Exception exception)
     {
         WriteExpectedException(exception);
-        Interop.MessageBox(exception.ToString(), "Expected exception");
+        Interop.MessageBox(exception.ToString(), "Korn expected exception");
     }
 
     public static void UnhandledException(Exception exception)
     {
         WriteUnhandledException(exception);
-        Interop.MessageBox(exception.ToString(), "Unhandled exception");
+        Interop.MessageBox(exception.ToString(), "Korn unhandled exception");
     }
+}
+
+public abstract class BaseKornException : Exception
+{
+    static BaseKornException() => KornLogger.EnsureInitialized();
+
+    public BaseKornException(string message, bool showMessageBox)
+        => (Message, ShowMessageBox) = (message, showMessageBox);
+
+    public BaseKornException(string[] messageLines, bool showMessageBox = true)
+        : this(string.Join(' ', messageLines), showMessageBox) { }
+
+    public new string Message;
+    public bool ShowMessageBox;
+}
+
+public class KornException : BaseKornException
+{
+    public KornException(string message, Exception exception, bool showMessageBox = true)
+        : base($"{message}: {exception}", showMessageBox) { }
+
+    public KornException(Exception exception, bool showMessageBox = true)
+        : base(exception.ToString(), showMessageBox) { }
+
+    public KornException(string message, bool showMessageBox = true)
+        : base(message, showMessageBox) { }
+
+    public KornException(string[] messageLines, bool showMessageBox = true)
+        : base(messageLines, showMessageBox) { }
+}
+
+public class KornExpectedException : BaseKornException
+{
+    public KornExpectedException(string message, Exception exception, bool showMessageBox = true)
+    : base($"{message}: {exception}", showMessageBox) { }
+
+    public KornExpectedException(Exception exception, bool showMessageBox = true)
+        : base(exception.ToString(), showMessageBox) { }
+
+    public KornExpectedException(string message, bool showMessageBox = true)
+        : base(message, showMessageBox) { }
+
+    public KornExpectedException(string[] messageLines, bool showMessageBox = true)
+        : base(messageLines, showMessageBox) { }
+}
+
+public class KornUnexpectedError : BaseKornException
+{
+    public KornUnexpectedError(string message, bool showMessageBox = true)
+        : base(message, showMessageBox) { }
+
+    public KornUnexpectedError(string[] messageLines, bool showMessageBox = true)
+        : base(messageLines, showMessageBox) { }
+}
+
+public class KornError : BaseKornException
+{
+    public KornError(string message, bool showMessageBox = true) 
+        : base(message, showMessageBox) { }
+
+    public KornError(string[] messageLines, bool showMessageBox = true)
+        : base(messageLines, showMessageBox) { }
 }
