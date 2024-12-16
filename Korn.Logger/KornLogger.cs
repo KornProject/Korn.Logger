372cb0d80bf206b669137c0e6e3b73d3a090606a
Yotic <yoticcitoy@gmail.com>
2024-12-16 20:51:00 +0300

    Big changes, local logger added.

--- Korn.Logger/KornLogger.cs
+++ Korn.Logger/KornLogger.cs
@@ -5,143 +5,141 @@ using System.Reflection;
 namespace Korn;
 public static class KornLogger
 {
-    static string? cachedKornLogPath;
-    static string? cachedTempKornLogPath;
-    const string KORN_PATH_VARIABLE_NAME = "KORN_PATH";
-    public static string GetLogFilePath()
+    public static void Write(string text) => Internal.Write(text);
+    public static void WriteLineWithoutTags(string text) => Internal.WriteLineWithoutTags(text);
+    public static void WriteLine(object obj) => Internal.WriteLine(obj is null ? "{null}" : obj.ToString() ?? "{null}", Assembly.GetCallingAssembly());
+    public static void WriteMessage(string message) => Internal.WriteMessage(message, Assembly.GetCallingAssembly());
+    public static void WriteException(Exception exception) => Internal.WriteException(exception, Assembly.GetCallingAssembly());
+    public static void WriteUnhandledException(Exception exception) => Internal.WriteUnhandledException(exception, Assembly.GetCallingAssembly());
+    public static void WriteExpectedException(Exception exception) => Internal.WriteExpectedException(exception, Assembly.GetCallingAssembly());
+    public static void WriteUnexpectedError(string errorMessage, bool appendStacktrace = false) => Internal.WriteUnexpectedError(errorMessage, appendStacktrace, Assembly.GetCallingAssembly());        
+    public static void WriteError(string errorMessage, bool appendStacktrace = false) => Internal.WriteLine("[Error] " + errorMessage + (appendStacktrace ? $"Stacktrace: \n{Environment.StackTrace}" : ""), Assembly.GetCallingAssembly());
+    public static void Message(string[] messageLines) => Internal.Message(messageLines, Assembly.GetCallingAssembly());
+    public static void Message(string message) => Internal.Message(message, Assembly.GetCallingAssembly());
+    public static void UnxepectedError(string[] messageLines) => Internal.UnxepectedError(messageLines, Assembly.GetCallingAssembly());
+    public static void UnxepectedError(string message) => Internal.UnxepectedError(message, Assembly.GetCallingAssembly());
+    public static void Error(string[] messageLines) => Internal.Error(messageLines, Assembly.GetCallingAssembly());
+    public static void Error(string message) => Internal.Error(message, Assembly.GetCallingAssembly());
+    public static void Exception(Exception exception) => Internal.Exception(exception, Assembly.GetCallingAssembly());
+    public static void ExpectedException(Exception exception) => Internal.ExpectedException(exception, Assembly.GetCallingAssembly());
+    public static void UnhandledException(Exception exception) => Internal.UnhandledException(exception, Assembly.GetCallingAssembly());
+
+    public static class Internal
     {
-        if (cachedKornLogPath is not null)
-            return cachedKornLogPath;
-
-        var kornPath = SystemVariablesUtils.GetVariable(KORN_PATH_VARIABLE_NAME);
-        if (kornPath is null)
-            return GetTempLogFilePath();
-
-        var logFilePath = Path.Combine(kornPath, "log.txt");
-        if (!File.Exists(logFilePath))
-            File.Create(logFilePath).Dispose();
-
-        return cachedKornLogPath = logFilePath;
-    }
-
-    static string GetTempLogFilePath()
-    {
-        if (cachedTempKornLogPath is not null)
-            return cachedTempKornLogPath;
-
-        var tempLogFileName = $"KornTempLog-{DateTime.Now.ToString("yy'_'MM'_'dd' 'HH'_'mm'_'ss")}";
-        var tempLogFilePath = Path.Combine(Path.GetTempPath(), tempLogFileName);
-
-        if (!File.Exists(tempLogFilePath))
-            File.Create(tempLogFilePath).Dispose();
-
-        return cachedTempKornLogPath = tempLogFilePath;
-    }
-
-    public static void HandleException(Exception exception)
-    {
-        switch (exception)
+        static Internal()
         {
-            case KornException kornException:
-                if (kornException.ShowMessageBox)
-                    Exception(kornException);
-                else WriteException(kornException);
-                break;
-
-            case KornExpectedException kornExpectedException:
-                if (kornExpectedException.ShowMessageBox)
-                    ExpectedException(kornExpectedException);
-                else WriteExpectedException(kornExpectedException);
-                break;
-
-            case KornUnexpectedError kornError:
-                if (kornError.ShowMessageBox)
-                    UnxepectedError(kornError.Message);
-                else WriteUnexpectedError(kornError.Message);
-                break;
-
-            case KornError kornExpectedError:
-                if (kornExpectedError.ShowMessageBox)
-                    Error(kornExpectedError.Message);
-                else WriteError(kornExpectedError.Message);
-                break;
-
-            default:
-                WriteUnhandledException(exception);
-                break;
+            LogPath =
+                (KornPath = SystemVariablesUtils.GetKornPath()) is not null
+                ? Path.Combine(KornPath, @"Data\log.txt")
+                : GetTempLogFilePath();
         }
-    }
 
-    static void InternalWrite(string text)
-    {
-        using (var mutex = new Mutex(false, "Korn logger mutex"))
+        static readonly string? KornPath;
+        static readonly string LogPath;
+
+        static string GetTempLogFilePath()
         {
-            mutex.WaitOne();
-            File.AppendAllText(GetLogFilePath(), text);
-            mutex.ReleaseMutex();
+            var tempLogFileName = $"KornTempLog-{DateTime.Now.ToString("yy'_'MM'_'dd' 'HH'_'mm'_'ss")}";
+            var tempLogFilePath = Path.Combine(Path.GetTempPath(), tempLogFileName);
+
+            if (!File.Exists(tempLogFilePath))
+                File.Create(tempLogFilePath).Dispose();
+
+            return tempLogFilePath;
         }
-    }
 
-    public static void Write(string text) => InternalWrite(text);
+        public static void InternalWrite(string text)
+        {
+            using (var mutex = new Mutex(false, "Korn logger mutex"))
+            {
+                mutex.WaitOne();
+                File.AppendAllText(LogPath, text);
+                mutex.ReleaseMutex();
+            }
+        }
 
-    public static void RawWriteLine(string text) => Write(text + '\n');
+        public static void HandleException(Exception exception, Assembly assembly)
+        {
+            switch (exception)
+            {
+                case KornException kornException:
+                    if (kornException.ShowMessageBox)
+                        Exception(kornException, assembly);
+                    else WriteException(kornException, assembly);
+                    break;
 
-    public static void WriteLine(object obj) => WriteLine(obj is null ? "{null}" : obj.ToString()??"{null}");
+                case KornExpectedException kornExpectedException:
+                    if (kornExpectedException.ShowMessageBox)
+                        ExpectedException(kornExpectedException, assembly);
+                    else WriteExpectedException(kornExpectedException, assembly);
+                    break;
+
+                case KornUnexpectedError kornError:
+                    if (kornError.ShowMessageBox)
+                        UnxepectedError(kornError.Message, assembly);
+                    else WriteUnexpectedError(kornError.Message, true, assembly);
+                    break;
+
+                case KornError kornExpectedError:
+                    if (kornExpectedError.ShowMessageBox)
+                        Error(kornExpectedError.Message, assembly);
+                    else WriteError(kornExpectedError.Message, true, assembly);
+                    break;
+
+                default:
+                    WriteUnhandledException(exception, assembly);
+                    break;
+            }
+        }
+
+        public static void Write(string text) => InternalWrite(text);
+        public static void WriteLineWithoutTags(string text) => Write(text + '\n');
+        public static void WriteLine(string text, Assembly assembly) => Write($"{DateTime.Now:yy/MM/dd HH:mm:ss.fff} [{assembly.GetName().Name}{(string.IsNullOrEmpty(Thread.CurrentThread.Name) ? "" : $"/{Thread.CurrentThread.Name}")}] " + text + '\n');
+        public static void WriteMessage(string message, Assembly assembly) => WriteLine(message, assembly);
+        public static void WriteException(Exception exception, Assembly assembly) => WriteLine("[Exception] " + exception.ToString(), assembly);
+        public static void WriteUnhandledException(Exception exception, Assembly assembly) => WriteLine("[Unhandled exception] " + exception.ToString(), assembly);
+        public static void WriteExpectedException(Exception exception, Assembly assembly) => WriteLine("[Expected exception] " + exception.ToString(), assembly);
+        public static void WriteUnexpectedError(string errorMessage, bool appendStacktrace, Assembly assembly) => WriteLine("[Unexpected error] " + errorMessage + (appendStacktrace ? $"Stacktrace: \n{Environment.StackTrace}" : ""), assembly);
+        public static void WriteError(string errorMessage, bool appendStacktrace, Assembly assembly) => WriteLine("[Error] " + errorMessage + (appendStacktrace ? $"Stacktrace: \n{Environment.StackTrace}" : ""), assembly);
+        public static void Message(string[] messageLines, Assembly assembly) => Message(string.Join('\n', messageLines), assembly);
         
-    public static void WriteLine(string text)
-        => Write($"{DateTime.Now:yy/MM/dd HH:mm:ss.fff} [{Assembly.GetExecutingAssembly().GetName().Name}/{Thread.CurrentThread.Name}] " + text + '\n');
+        public static void Message(string message, Assembly assembly)
+        {
+            WriteMessage(message, assembly);
+            Interop.MessageBox(message, "Korn message");
+        }
 
-    public static void WriteMessage(string message) => WriteLine("[Message] " + message);
+        public static void UnxepectedError(string[] messageLines, Assembly assembly) => UnxepectedError(string.Join(' ', messageLines), assembly);
+        public static void UnxepectedError(string message, Assembly assembly)
+        {
+            WriteUnexpectedError(message, true, assembly);
+            Interop.MessageBox(message, "Korn unexpected error");
+        }
 
-    public static void WriteException(Exception exception) => WriteLine("[Exception] " + exception.ToString());
+        public static void Error(string[] messageLines, Assembly assembly) => Error(string.Join(' ', messageLines), assembly);
+        public static void Error(string message, Assembly assembly)
+        {
+            WriteError(message, false, assembly);
+            Interop.MessageBox(message, "Korn error");
+        }
 
-    public static void WriteUnhandledException(Exception exception) => WriteLine("[Unhandled exception] " + exception.ToString());
+        public static void Exception(Exception exception, Assembly assembly)
+        {
+            WriteException(exception, assembly);
+            Interop.MessageBox(exception.ToString(), "Korn exception");
+        }
 
-    public static void WriteExpectedException(Exception exception) => WriteLine("[Expected exception] " + exception.ToString());
+        public static void ExpectedException(Exception exception, Assembly assembly)
+        {
+            WriteExpectedException(exception, assembly);
+            Interop.MessageBox(exception.ToString(), "Korn expected exception");
+        }
 
-    public static void WriteUnexpectedError(string errorMessage, bool appendStacktrace = false)
-        => WriteLine("[Unexpected error] " + errorMessage + (appendStacktrace ? $"Stacktrace: \n{Environment.StackTrace}" : ""));
-
-    public static void WriteError(string errorMessage, bool appendStacktrace = false)
-    => WriteLine("[Error] " + errorMessage + (appendStacktrace ? $"Stacktrace: \n{Environment.StackTrace}" : ""));
-
-    public static void Message(string[] messageLines) => Message(string.Join('\n', messageLines));
-    public static void Message(string message)
-    {
-        WriteMessage(message);
-        Interop.MessageBox(message, "Korn Message");
-    }
-
-    public static void Error(string[] messageLines) => UnxepectedError(string.Join(' ', messageLines));
-    public static void UnxepectedError(string message)
-    {
-        WriteUnexpectedError(message);
-        Interop.MessageBox(message, "Korn unexpected error");
-    }
-
-    public static void ExpectedError(string[] messageLines) => Error(string.Join(' ', messageLines));
-    public static void Error(string message)
-    {
-        WriteError(message);
-        Interop.MessageBox(message, "Korn error");
-    }
-
-    public static void Exception(Exception exception)
-    {
-        WriteException(exception);
-        Interop.MessageBox(exception.ToString(), "Korn exception");
-    }
-
-    public static void ExpectedException(Exception exception)
-    {
-        WriteExpectedException(exception);
-        Interop.MessageBox(exception.ToString(), "Korn expected exception");
-    }
-
-    public static void UnhandledException(Exception exception)
-    {
-        WriteUnhandledException(exception);
-        Interop.MessageBox(exception.ToString(), "Korn unhandled exception");
+        public static void UnhandledException(Exception exception, Assembly assembly)
+        {
+            WriteUnhandledException(exception, assembly);
+            Interop.MessageBox(exception.ToString(), "Korn unhandled exception");
+        }
     }
 }
 
@@ -150,24 +148,28 @@ public abstract class BaseKornException : Exception
     public BaseKornException(string[] messageLines, bool showMessageBox = true)
         : this(string.Join(' ', messageLines), showMessageBox) { }
 
-    public BaseKornException(string message, bool showMessageBox = true)
-    {
-        (Message, ShowMessageBox) = (message, showMessageBox);
+    public BaseKornException(Exception exception, bool showMessageBox = true)
+        : base("Empty exception message", exception) { }
 
-        KornLogger.HandleException(this);
+    public BaseKornException(string message, Exception exception, bool showMessageBox = true)
+        : base(message, exception) { }
+
+    public BaseKornException(string message, bool showMessageBox = true) : base(message)
+    {
+        ShowMessageBox = showMessageBox;
+        KornLogger.Internal.HandleException(this, Assembly.GetCallingAssembly());
     }
 
-    public new string Message;
     public bool ShowMessageBox;
 }
 
 public class KornException : BaseKornException
 {
     public KornException(string message, Exception exception, bool showMessageBox = true)
-        : base($"{message}: {exception}", showMessageBox) { }
+        : base(message, exception, showMessageBox) { }
 
     public KornException(Exception exception, bool showMessageBox = true)
-        : base(exception.ToString(), showMessageBox) { }
+        : base(exception, showMessageBox) { }
 
     public KornException(string message, bool showMessageBox = true)
         : base(message, showMessageBox) { }
