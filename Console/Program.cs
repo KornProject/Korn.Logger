df6646924f458c0b1731de2cc6c77745e5b5ebbf
Yotic <yoticcitoy@gmail.com>
2025-03-17 18:44:39 +0300

    Minor changes. The commit before refactoring.

---
+++ Console/Program.cs
@@ -0,0 +1,29 @@
+﻿using Korn;
+
+try
+{
+    var logger = new KornLogger("temp.log");
+    logger.Clear();
+
+    var thread1 = new Thread(Body);
+    var thread2 = new Thread(Body);
+
+    thread1.Start();
+    thread2.Start();
+
+    thread1.Join();
+    thread2.Join();
+
+    void Body()
+    {
+        for (var i = 0; i < 50000; i++)
+            logger.WriteMessage(i.ToString());
+    }
+} 
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+
+Console.WriteLine("end");
+Console.ReadLine();
--- Korn.Logger/KornLogger.cs
+++ Korn.Logger/KornLogger.cs
@@ -1,9 +1,12 @@
 ﻿using Korn.Logger.Internal;
+using Korn.Shared.Internal;
 using System;
 using System.Diagnostics;
 using System.IO;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Korn
 {
@@ -19,12 +22,12 @@ namespace Korn
 
         internal Internal Implementation;
 
-
         public void Write(string text) => Implementation.Write(text);
+        public void Clear() => Implementation.Clear();
         public void WriteLineWithoutTags(string text) => Implementation.WriteLineWithoutTags(text);
         public void WriteLine(object obj) => Implementation.WriteLine(obj is null ? "{null}" : obj.ToString() ?? "{null}", Assembly.GetCallingAssembly());
         public void WriteMessage(string message) => Implementation.WriteMessage(message, Assembly.GetCallingAssembly());
-        public void WriteWarning(string message) => Implementation.WriteWarning(message, Assembly.GetCallingAssembly());
+        public void WriteWarning(params string[] message) => Implementation.WriteWarning(message, Assembly.GetCallingAssembly());
         public void WriteException(Exception exception) => Implementation.WriteException(exception, Assembly.GetCallingAssembly());
         public void WriteUnhandledException(Exception exception) => Implementation.WriteUnhandledException(exception, Assembly.GetCallingAssembly());
         public void WriteExpectedException(Exception exception) => Implementation.WriteExpectedException(exception, Assembly.GetCallingAssembly());
@@ -56,14 +59,27 @@ namespace Korn
                     mutex.WaitOne();
                     File.AppendAllText(LogPath, text);
                     mutex.ReleaseMutex();
-                }                    
+                }
             }
 
+            void InternalClear()
+            {
+                using (var mutex = new Mutex(false, $"{LogPath.GetHashCode():X}lm"))
+                {
+                    mutex.WaitOne();
+                    File.WriteAllText(LogPath, "");
+                    mutex.ReleaseMutex();
+                }
+            }
+
+            string GetThreadTag() => string.IsNullOrEmpty(Thread.CurrentThread.Name) ? "" : Thread.CurrentThread.Name == ".NET TP Worker" ? "" : $"/{Thread.CurrentThread.Name}";
+
+            public void Clear() => InternalClear();
             public void Write(string text) => InternalWrite(text);
             public void WriteLineWithoutTags(string text) => Write(text + '\n');
-            public void WriteLine(string text, Assembly assembly) => Write($"{DateTime.Now:yy/MM/dd HH:mm:ss.fff} {InstanceID} [{assembly.GetName().Name}{(string.IsNullOrEmpty(Thread.CurrentThread.Name) ? "" : $"/{Thread.CurrentThread.Name}")}] " + text + '\n');
+            public void WriteLine(string text, Assembly assembly) => Write($"{DateTime.Now:yy/MM/dd HH:mm:ss.fff} {InstanceID} [{assembly.GetName().Name}{GetThreadTag()}] " + text + '\n');
             public void WriteMessage(string message, Assembly assembly) => WriteLine(message, assembly);
-            public void WriteWarning(string message, Assembly assembly) => WriteLine("[Warning] " + message, assembly);
+            public void WriteWarning(string[] message, Assembly assembly) => WriteLine("[Warning] " + string.Join(" ", message), assembly);
             public void WriteException(Exception exception, Assembly assembly) => WriteLine("[Exception] " + exception.ToString(), assembly);
             public void WriteUnhandledException(Exception exception, Assembly assembly) => WriteLine("[Unhandled exception] " + exception.ToString(), assembly);
             public void WriteExpectedException(Exception exception, Assembly assembly) => WriteLine("[Expected exception] " + exception.ToString(), assembly);
@@ -141,12 +157,25 @@ namespace Korn
 
     public abstract class BaseKornException : Exception
     {
+        static BaseKornException()
+        {
+            var assembly = AppDomain.CurrentDomain.Load("Korn.Shared");
+            if (assembly == null)
+                return;
+
+            var type = assembly.GetType("Korn.Shared.KornShared");
+            if (type == null)
+                return;
+
+            RuntimeHelpers.RunClassConstructor(type.TypeHandle);
+        }
+
         static KornLogger ExceptionLogger;
         public static bool HasBindedLogger() => ExceptionLogger != null;
         public static void BindLogger(KornLogger logger) => ExceptionLogger = logger;
 
         public BaseKornException(string[] messageLines, bool showMessageBox = true)
-            : this(string.Join(" ", messageLines), showMessageBox) { }
+            : this(string.Join(string.Empty, messageLines), showMessageBox) { }
 
         public BaseKornException(Exception exception, bool showMessageBox = true)
             : base("Empty exception message", exception) { }
